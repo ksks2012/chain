93605f119d0aabaebd800b298cbe2a4511a82147
ksks2012 <st890751999@gmail.com>
2021-10-25 01:48:39 +0800

    feat(hashing): add hash method

---
+++ pkg/hashing/hasher.go
@@ -0,0 +1,29 @@
+package hashing
+
+import (
+	"crypto/sha1"
+
+	"github.com/block-chain/global"
+)
+
+func transactionToString(transaction global.Transaction) (transactionstring string) {
+	transactionstring = transaction.Sender + transaction.Receiver + transaction.Amounts + transaction.Fee + transaction.Message
+	return
+}
+
+func getTransactionsString(block global.Block) (transactionsString string) {
+	transactionsString = ""
+	for _, transaction := range block.Transactions {
+		transactionsString += transactionToString(transaction)
+	}
+	return
+}
+
+func getHash(block global.Block, nonce string) (bs []byte) {
+	sha1Hasher := sha1.New()
+	hashInput := block.Previous_hash + block.Timestamp.String() + getTransactionsString(block) + nonce
+	sha1Hasher.Write([]byte(hashInput))
+	bs = sha1Hasher.Sum(nil)
+	return
+
+}
